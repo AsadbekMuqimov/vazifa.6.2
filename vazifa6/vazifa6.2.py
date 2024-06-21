4c036bb85742564e1d5e3d3bed0eae6b1b7e8be6
AsadbekMuqimov <muqimovasadbek04@gmail.com>
2024-06-21 20:27:02 +0500

    first commit

---
+++ vazifa6/vazifa6.2.py
@@ -0,0 +1,27 @@
+"""from django.urls import path
+from . import views
+
+urlpatterns = [
+       path('open_port/', views.port_och, name='port_och'),
+   ]"""
+   
+"""from django.shortcuts import render
+from django.http import httpResponse
+
+def port_ochish(request):
+       return render(request, 'port_och.html')"""
+
+
+"""<!DOCTYPE html>
+<html>
+ <head>
+  <title>Port Ochish</title>
+ </head>
+ <body>
+    <h1>Port Ochish</h1>
+       <p>Bu HTML fayl orqali port ochilmoqda</p>
+ </body>
+</html>"""
+   
+   
+   
